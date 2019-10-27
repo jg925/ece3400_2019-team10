667b3361edb42e054473278be05b8b54b72be316
Joy Thean <jct263@cornell.edu>
2019-10-27 13:17:09 -0400

    changes;

--- labs/lab3/phototransistor_detection/phototransistor_detection.ino
+++ labs/lab3/phototransistor_detection/phototransistor_detection.ino
@@ -5,11 +5,13 @@ Servo left;
 Servo right;
 
 // servo pins
-int right_pin = 6;
-int left_pin = 5;
+int right_pin = 5;
+int left_pin = 6;
 
 // phototransistor pins
-int photoPin = A1;
+int photoPin = A3;
+
+int pi_arr[10];
 
 void halt() {
   left.write(90);
@@ -31,14 +33,27 @@ void setup() {
 
 void loop() {
   int photo_input = analogRead(photoPin);
-  Serial.println(photo_input);
-  delay(100);
+  //Serial.println(photo_input);
+  //delay(10000);
 
-  if (photo_input >= 50) {
-    //moveForward();
-    Serial.println("DETECT");
-  } else {
+  pi_arr[9] = photo_input;
+
+  Serial.println("ARRAY START");
+  for (int i= 0; i < 10; i++) {
+    Serial.println(pi_arr[i]);
+  }
+  Serial.println("ARRAY END");
+  delay(1000);
+  
+  for (int i= 1; i < 10; i++) {
+    pi_arr[i-1] = pi_arr[i];
+  }
+  
+  if (photo_input >= 5) {
     //halt();
+    //Serial.println("DETECT");
+  } else {
+    //moveForward();
   }
   
 }
