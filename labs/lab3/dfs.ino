c71750f762885f5a5f1c45449ad876adb9f0fdac
Daniel Joseph Buscaglia <djb326@cornell.edu>
2019-11-02 15:26:17 -0400

    Update dfs.ino

--- labs/lab3/dfs.ino
+++ labs/lab3/dfs.ino
@@ -83,7 +83,7 @@ void right180Turn() {
 //
 //  // if both sensors on white
 //  if (left_sensor_value < threshold && right_sensor_value < threshold) {
-//    
+//
 //    // to move into corresponding turn cases after debug
 //    //halt();
 //    //delay(1000);
@@ -98,7 +98,7 @@ void right180Turn() {
 //
 //    Serial.println("DISTANCE");
 //    Serial.println(ldistance);
-//  
+//
 //    if (ldistance <= 10) {
 //      left_detect = 0;
 //    } else {
@@ -109,7 +109,7 @@ void right180Turn() {
 //    float fvolts = analogRead(front_ir_sensor)*0.0048828125;  // value from sensor * (5/1024)
 //    int fdistance = 13*pow(fvolts, -1); // worked out from datasheet graph
 //    //delay(1000); // slow down serial port
-//  
+//
 //    if (fdistance <= 20) {
 //      front_detect = 0;
 //    } else {
@@ -118,10 +118,10 @@ void right180Turn() {
 //
 //    // right IR sensor read + distance calculation
 //    right_detect = digitalRead(right_ir_sensor);  // 0 when detecting
-//  
+//
 //    // left IR sensor read + distance calculation
 //    //left_detect = digitalRead(right_ir_sensor);   // 0 when detecting
-//  
+//
 //    // front IR sensor read + distance calculation
 //    //front_detect = digitalRead(front_ir_sensor);  // 0 when detecting
 //
@@ -138,13 +138,13 @@ void right180Turn() {
 //      Serial.println("NO WALLS");
 //      right90Turn();
 //    }
-//    
+//
 //    // case 1: right wall, left wall, front wall --> turn around
 //    else if (!right_detect && !left_detect && !front_detect) {
 //      Serial.println("180 TURN");
 //      right180Turn();
 //    }
-//    
+//
 //    // case 2: left wall, front wall --> turn right
 //    else if (right_detect && !left_detect && !front_detect) {
 //      Serial.println("RIGHT TURN");
@@ -166,23 +166,23 @@ void right180Turn() {
 //    else if (right_detect && !left_detect && front_detect) {
 //      right90Turn();
 //    }
-//    
+//
 //    // case 6: otherwise --> go straight
 //    else {
 //      Serial.println("STRIGHT");
 //      moveForward();
 //    }
 //  }
-//  
+//
 //  // left on white, right on black --> slight left adjust
 //  else if (left_sensor_value < threshold && right_sensor_value >= threshold) {
 //    // left detects white, right detects black
 //    while (left_sensor_value < threshold) {
 //      slightLeft();
 //      left_sensor_value = analogRead(LEFT_LINE_SENSOR);
-//    }    
+//    }
 //  }
-//  
+//
 //  // right on white, left on black --> slight right adjust
 //  else if (left_sensor_value >= threshold && right_sensor_value < threshold) {
 //    // left detects black, right detects white
@@ -191,7 +191,7 @@ void right180Turn() {
 //      right_sensor_value = analogRead(RIGHT_LINE_SENSOR);
 //    }
 //  }
-//  
+//
 //  // both sensors detect black --> go straight
 //  else if (left_sensor_value >= threshold && right_sensor_value >= threshold) {
 //    Serial.println("FORWARD");
@@ -202,7 +202,7 @@ void right180Turn() {
 //void loop() {
 //  // put your main code here, to run repeatedly:
 //  //halt();
-//  
+//
 //  navigate();
 //}
 
@@ -221,122 +221,218 @@ struct node {
   byte frontier[3];
 };
 
+struct nodesmall {
+  byte x;
+  byte y;
+  direction dir;
+};
+
 node visited [100];
-int i = 0;
+nodesmall path [100];
+int visitedi = 0;
+int pathi = 0;
 node current;
 node avail;
+int clr[3] = { 1, 0, 2 };
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin( 9600 );
 
-  pinMode(LEFT_LINE_SENSOR, INPUT);
-  pinMode(RIGHT_LINE_SENSOR, INPUT);
-  pinMode(left_ir_sensor, INPUT);
-  pinMode(front_ir_sensor, INPUT);
-  pinMode(right_ir_sensor, INPUT);
-  right.attach(right_pin);
-  left.attach(left_pin);
+  pinMode( LEFT_LINE_SENSOR, INPUT );
+  pinMode( RIGHT_LINE_SENSOR, INPUT );
+  pinMode( left_ir_sensor, INPUT );
+  pinMode( front_ir_sensor, INPUT );
+  pinMode( right_ir_sensor, INPUT );
+  right.attach( right_pin );
+  left.attach( left_pin );
 
   // init starting position
-  current = { B0, B0, north, B111, {B0,B0,B0} };
-
+  current = { B0, B0, north, B111, { B0, B0, B0 } }; // Must use B111 because we can't shift in 1's.
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  int right_detect = digitalRead(right_ir_sensor);
-  int left_detect = digitalRead(left_ir_sensor);
-  int front_detect = digitalRead(front_ir_sensor);
-
-  visited[i] = current;
-  i++;
 
   // wall detection
+  int right_detect = digitalRead( right_ir_sensor );
+  int left_detect = digitalRead( left_ir_sensor );
+  int front_detect = digitalRead( front_ir_sensor );
 
-  if (left_detect) {
+  if ( left_detect ) {
     current.walls = current.walls << 1;
   }
-
   current.walls = current.walls << 1;
-  if (!front_detect) {
+  if ( !front_detect ) {
     current.walls = current.walls + B001;
   }
-
   current.walls = current.walls << 1;
-  if (!right_detect) {
+  if ( !right_detect ) {
     current.walls = current.walls + B001;
   }
 
   int w;
-
-  // left wall not detected, add neighbor to frontier set
-  if (!((current.walls & B100) && B100)) {
-    node avail = { current.x-B1, current.y, west, B111 };
-    for (w = 0; w < i; w++)
+  // left wall not detected
+  if ( !( ( current.walls & B100 ) && B100 ) ) {
+    nodesmall avail = { current.x - B1, current.y, direction( ( int( current.dir ) - 1 ) % 4 ) };  // I don't like this. What if we start at _|? Then we are working with negative coordinates.
+    for (w = 0; w < visitedi; w++)
     {
-      if (avail.x == visited[w].x && avail.y==visited[w].y){
+      if ( avail.x == visited[w].x && avail.y == visited[w].y ) {
         break;
       }
     }
-    if (w == i) {
+    if ( w == visitedi ) { // if not found in visited set, add to frontier
       current.frontier[1] = avail.x << 4 + avail.y;
     }
   }
 
-  // front wall not detected, add neighbor to frontier set
-  if (!((current.walls & B010) && B010)) {
-    node avail = { current.x, current.y+B1, north, B111 };
-    for (w = 0; w < i; w++)
+  // front wall not detected
+  if ( !( ( current.walls & B010 ) && B010 ) ) {
+    nodesmall avail = { current.x, current.y + B1, current.dir }; // See above
+    for (w = 0; w < visitedi; w++)
     {
-      if (avail.x == visited[w].x && avail.y==visited[w].y){
+      if ( avail.x == visited[w].x && avail.y == visited[w].y ) {
         break;
       }
     }
-    if (w == i) {
+    if ( w == visitedi ) { // if not found in visited set, add to frontier
       current.frontier[1] = avail.x << 4 + avail.y;
     }
   }
-  
-  // right wall not detected, add neighbor to frontier set
-  if (!((current.walls & B001) && B001)) {
-    node avail = { current.x+B1, current.y, east, B111 };
-    for (w = 0; w < i; w++)
+
+  // right wall not detected
+  if ( !( ( current.walls & B001 ) && B001 ) ) {
+    nodesmall avail = { current.x + B1, current.y, direction( ( int( current.dir ) + 1 ) % 4 ) }; // See above
+    for ( w = 0; w < visitedi; w++ )
     {
-      if (avail.x == visited[w].x && avail.y==visited[w].y){
+      if ( avail.x == visited[w].x && avail.y == visited[w].y ) {
         break;
       }
     }
-    if (w == i) {
+    if ( w == visitedi ) { // if not found in visited set, add to frontier
       current.frontier[2] = avail.x << 4 + avail.y;
     }
   }
 
-  int q;
-  for (q = 0; q < 3; q++){
-    if (current.frontier[q] != B0){
-      byte nextx = current.frontier[q]>>4;
-      byte nexty = (current.frontier[q] << 4) >> 4;
+  // Don't want to add repeats into visited node
 
-      if (q == 0) {
-        turnLeft();
-        face = current.dir - 1;
-      }
-      else if (q == 2) {
-        turnRight();
-        face = current.dir + 1;
-      } else {
+  byte flag = B0;
+  for ( int z = 0; z < visitedi; z++ ) {
+    if ( visited[z].x == current.x && visited[z].y == current.y ) {
+      flag = B1;
+      break;
+    }
+  }
+  if ( flag == B0 || visitedi == 0 ) {
+    visited[visitedi] = current;
+    visitedi++;
+  }
+
+  // Next direction, next place to move
+  int q;
+  byte nextx;
+  byte nexty;
+  direction face = current.dir;
+  for ( q = 0; q < 3; q++ ) {
+    int temp = clr[q];
+    if ( current.frontier[temp] != B0 ) {
+      nextx = current.frontier[temp] >> 4;
+      nexty = ( current.frontier[temp] << 4 ) >> 4;
+
+      if (temp == 1) {                     // open space in front of robot
         face = current.dir;
+        if ( current.dir == north ) {
+          nextx = current.x;
+          nexty = current.y + B1;
+        }
+        else if ( current.dir == east) {
+          nextx = current.x + B1;
+          nexty = current.y;
+        }
+        else if ( current.dir == south ) {
+          nextx = current.x;
+          nexty = current.y - B1;
+        }
+        else {
+          nextx = current.x - B1;
+          nexty = current.y ;
+        }
+      }
+      else if (temp == 0) {                // open space to left of robot
+        face = direction( ( int( current.dir ) - 1 ) % 4 );
+        if ( current.dir == north ) {
+          nextx = current.x - B1;
+          nexty = current.y;
+        }
+        else if ( current.dir == east) {
+          nextx = current.x;
+          nexty = current.y + B1;
+        }
+        else if ( current.dir == south ) {
+          nextx = current.x + B1;
+          nexty = current.y;
+        }
+        else {
+          nextx = current.x;
+          nexty = current.y - B1;
+        }
+        left90Turn();
+      }
+      else if (temp == 2) {                // open space to right of robot
+        face = direction( ( int( current.dir ) + 1 ) % 4 );
+        if ( current.dir == north ) {
+          nextx = current.x + B1;
+          nexty = current.y;
+        }
+        else if ( current.dir == east) {
+          nextx = current.x;
+          nexty = current.y - B1;
+        }
+        else if ( current.dir == south ) {
+          nextx = current.x - B1;
+          nexty = current.y;
+        }
+        else {
+          nextx = current.x;
+          nexty = current.y + B1;
+        }
+        right90Turn();
       }
       moveForward();  // change to navigate later
       break;
     }
   }
   if (q == 3) {
-    right180Turn();
     // nextx and nexty --> node behind the robot
-    face = current.dir + 2;  
+    face = direction( ( int( current.dir ) + 2 ) % 4 );
+    byte return_to_x = path[pathi].x;
+    byte return_to_y = path[pathi].y;
+    direction return_to_dir = path[pathi].dir;
+    right180Turn();
+    
+//    if ( current.dir == north ) {
+//      nextx = current.x;
+//      nexty = current.y - B1;
+//    }
+//    else if ( current.dir == east) {
+//      nextx = current.x - B1;
+//      nexty = current.y;
+//    }
+//    else if ( current.dir == south ) {
+//      nextx = current.x;
+//      nexty = current.y + B1;
+//    }
+//    else {
+//      nextx = current.x + B1;
+//      nexty = current.y ;
+//    }
+    //moveForward();
   }
-  current = {nextx, nexty, face, B111, {B0, B0, B0} };
+  else {
+    path[pathi].x = current.x;
+    path[pathi].y = current.y;
+    path[pathi].dir = current.dir;
+    pathi++;
+  }
+
+  current = { nextx, nexty, face, B111, { B0, B0, B0 } };
 }
