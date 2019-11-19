be1e39fe95e5c7cad32e659bf8a832c458265064
Ryan McMahon <rm756@cornell.edu>
2019-11-18 20:52:52 -0500

    Continued integration

--- labs/lab4/RobotIntegration/RobotIntegration.ino
+++ labs/lab4/RobotIntegration/RobotIntegration.ino
@@ -224,6 +224,71 @@ int navigate() {
 
 // =====================================================================================================
 
+// FFT
+
+// =====================================================================================================
+
+#define LOG_OUT 1 // use the log output function
+#define FFT_N 256 // set to 256 point fft
+
+#include <FFT.h> // include the library
+
+int detect_count = 0;
+
+int is_maximum( int five, int six, int seven, int eight, int FFT_threshold ) {
+  if ( six > FFT_threshold && seven > FFT_threshold ) {
+    //if ( six > five && six > eight && seven > five && seven > eight ) {
+      // checking that six and seven are a local maximum
+      //if ( six - seven < 10 && six - five > 10 && six - seven > 0) {
+        // checking that shape of curve is correct
+        return 1;
+      //}
+    //}
+  }
+  else {
+    return 0;
+  }
+}
+
+void fft() {
+  while (1) { // reduces jitter
+    cli();  // UDRE interrupt slows this way down on arduino1.0
+    for (int i = 0 ; i < 512 ; i += 2) { // save 256 samples
+      while (!(ADCSRA & 0x10)); // wait for adc to be ready
+      ADCSRA = 0xf5; // restart adc
+      byte m = ADCL; // fetch adc data
+      byte j = ADCH;
+      int k = (j << 8) | m; // form into an int
+      k -= 0x0200; // form into a signed int
+      k <<= 6; // form into a 16b signed int
+      fft_input[i] = k; // put real data into even bins
+      fft_input[i + 1] = 0; // set odd bins to 0
+    }
+    fft_window(); // window the data for better frequency response
+    fft_reorder(); // reorder the data before doing the fft
+    fft_run(); // process the data in the fft
+    fft_mag_log(); // take the output of the fft
+    sei();
+    int max = is_maximum( fft_log_out[5], fft_log_out[6], fft_log_out[7], fft_log_out[8], 124 );
+    if (max == 1 && detect_count >= 6)
+    {
+      Serial.println("950 Hz");
+      detect_count = 0;
+      break;
+      //Serial.println(detect_count);
+    }
+    else if (digitalRead(START_BUTTON)) {
+      Serial.println("Button Press");
+      break;
+    }
+    else if ( max == 1 ) {
+      detect_count++;
+    }
+  }
+}
+
+// =====================================================================================================
+
 // communication
 
 // =====================================================================================================
@@ -517,7 +582,7 @@ void setup() {
   pinMode(DONE_LED, OUTPUT);
 
   // serial and print setup
-  Serial.begin(9600);
+  Serial.begin(57600);
   printf_begin();
 
   // servo setup
@@ -535,6 +600,15 @@ void setup() {
     maze[i].walls = B00000111;  // initialize all walls to be 111
   }
   maze[0].visited = B00000001;
+
+  //
+  // FFT Setup
+  //
+  
+  TIMSK0 = 0; // turn off timer0 for lower jitter
+  ADCSRA = 0xe5; // set the adc to free running mode
+  ADMUX = 0x40; // use adc0
+  DIDR0 = 0x01; // turn off the digital input for adc0
 
   //
   // communication setup
@@ -583,14 +657,13 @@ void setup() {
 
 void loop() {
   while (beginning) { // to wait for pushbutton/950 Hz tone
-    if (digitalRead(START_BUTTON)) {
-      beginning = 0;
-      for (int i = 0; i < 2; i++) { // Signal we are about to begin
-        digitalWrite(DONE_LED, HIGH);
-        delay(500);
-        digitalWrite(DONE_LED, LOW);
-        delay(500);
-      }
+    fft();
+    beginning = 0;
+    for (int i = 0; i < 2; i++) { // Signal we are about to begin
+      digitalWrite(DONE_LED, HIGH);
+      delay(500);
+      digitalWrite(DONE_LED, LOW);
+      delay(500);
     }
   }
   
